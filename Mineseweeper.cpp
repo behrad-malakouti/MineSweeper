a0f16b3c54244aa11b93c81fad5675093aaffe29
behrad-malakouti <behradmprogarmer@gmail.com>
2019-01-07 17:34:45 +0330

    New version of MineSweeper source code added!

--- Mineseweeper.cpp
+++ Mineseweeper.cpp
@@ -2,63 +2,67 @@
 
 using namespace std;
 
-//The Function that set the map of the Matrix(game map)
-//Number 100 in this codes means Bomb
-//ERRORS: SomeTimes this algorithm generates 0 bombs and Sometimes it Doesn't show the True Number of the Bombs around the home
-static void set_map(int a, int b)
-{
-	srand(time(0));
-	int m[a][b], s, i2, j2;
 
-	for(int i = 0; i < a; i++)
+static void set_map()
+{
+	srand(time(NULL));
+	int m[9][9], s, i2, j2, bomb = 0;
+
+	while(bomb < 10)
 	{
-		i2 = rand() % a;
-		j2 = rand() % b;
-		for(int j = 0; j < b; j++)
-			if(i == i2 && j == j2)
-				m[i][j] = 100;
+		i2 = rand() % 9;
+		j2 = rand() % 9;
+
+		if(m[i2][j2] != 100)
+		{
+			m[i2][j2] = 100;
+			bomb++;
+		}
 	}
 
-	//show the Number of Bombs around the home
-	for(int i = 0; i < a; i++)
+	for(int i = 0; i < 9; i++)
 	{
 		s = 0;
-		for(int j = 0; j < b; j++)
+		for(int j = 0; j < 9; j++)
 		{
 			s = 0;
 			if(m[i][j] != 100)
 			{
-				if(m[i][j + 1] == 100)
-					s++;
-				if(m[i][j - 1] == 100)
-					s++;
-				if(m[i + 1][j] == 100)
-					s++;
-				if(m[i - 1][j] == 100)
-					s++;
+				if(j + 1 <= 8)
+					if(m[i][j + 1] == 100)
+						s++;
+
+				if(j - 1 >= 0)
+					if(m[i][j - 1] == 100)
+						s++;
+
+				if(i + 1 <= 8)
+					if(m[i + 1][j] == 100)
+						s++;
+
+				if(i - 1 >= 0)
+					if(m[i - 1][j] == 100)
+						s++;
 
 				m[i][j] = s;
 			}
 		}
 	}
 
-	//Showing the map in output
-	for(int i = 0; i < a; i++)
+	for(int i = 0; i < 9; i++)
 	{
-		for(int j = 0; j < b; j++)
-			cout << m[i][j] << "\t";
+		for(int j = 0; j < 9; j++)
+			if(m[i][j] != 100)
+				cout << m[i][j] << "\t";
+			else
+				cout << "BOMB\t";
 		cout << endl;
 	}
 }
 
 int main()
 {
-	int a, b;
-	cout << "a: ";
-	cin >> a;
-	cout << "b: ";
-	cin >> b;
+	set_map();
 
-	set_map(a, b);
 	return 0;
 }
